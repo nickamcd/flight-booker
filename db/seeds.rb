6c11e5d779ab0b228cc08eead8cbb326d127a788
Nick McDaniel <nickamcd@gmail.com>
2021-03-08 15:55:26 -0800

    Seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do
+  Airport.destroy_all
+  Flight.destroy_all
+end
+
+airports = Airport.create([
+  { code: 'SFO' },
+  { code: 'NYC' }
+])
+
+flights = Flight.create([
+  { from_airport_id: airports.first.id, to_airport_id: airports.last.id, duration: 300, takeoff_datetime: DateTime.now + 1 },
+  { from_airport_id: airports.first.id, to_airport_id: airports.last.id, duration: 300, takeoff_datetime: DateTime.now + 3 },
+  { from_airport_id: airports.first.id, to_airport_id: airports.last.id, duration: 300, takeoff_datetime: DateTime.now + 5 },
+  { from_airport_id: airports.last.id, to_airport_id: airports.first.id, duration: 360, takeoff_datetime: DateTime.now + 2 },
+  { from_airport_id: airports.last.id, to_airport_id: airports.first.id, duration: 360, takeoff_datetime: DateTime.now + 4 },
+  { from_airport_id: airports.last.id, to_airport_id: airports.first.id, duration: 360, takeoff_datetime: DateTime.now + 6 }
+])

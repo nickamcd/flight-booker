9d3d4c6c570d1ee5bfee47dcdcec7c810c88a734
Nick McDaniel <nickamcd@gmail.com>
2021-03-09 10:15:46 -0800

    Build basic form for searching flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
   def index
+    @airport_options  =   Airport.all.map { |a| [a.code, a.id] }
+    @date_options     =   Flight.all.map  { |f| f.takeoff_datetime.to_date }.uniq.sort
 
   end
 end

0708f517216a1f5d95730fe422080506bb6b28dc
Nick McDaniel <nickamcd@gmail.com>
2021-03-09 13:15:07 -0800

    Search function finally works

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,14 @@
 class FlightsController < ApplicationController
   def index
-    @airport_options  =   Airport.all.map { |a| [a.code, a.id] }
-    @date_options     =   Flight.all.map  { |f| f.takeoff_datetime.to_date }.uniq.sort
+    @airport_options = Airport.all.map { |a| [a.code, a.id] }
+    @date_options = Flight.all.map  { |f| f.takeoff_datetime.to_date }.uniq.sort
 
+    @available_flights = Flight.search(params[:flight])
+  end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:from_airport_id, :to_airport_id, :takeoff_datetime, :passenger_count)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,16 @@
 class Flight < ApplicationRecord
   belongs_to  :from_airport,  class_name: 'Airport',  foreign_key: 'from_airport_id'
   belongs_to  :to_airport,    class_name: 'Airoprt',  foreign_key: 'to_airport_id'
+
+  def self.search(search)
+    if search
+      flights = Flight.all
+      flights = flights.where(from_airport_id:  search[:from_airport_id], 
+                              to_airport_id:    search[:to_airport_id],
+                              takeoff_datetime: Date.parse(search[:takeoff_datetime]).all_day)
+    else
+      flights = Flight.all
+    end
+    flights
+  end
 end

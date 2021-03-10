a910bf1e56f3a74a97c90d49675949e801a283a6
Nick McDaniel <nickamcd@gmail.com>
2021-03-10 13:27:44 -0800

    Created associations between Passenger, Booking, and Flight

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to  :from_airport,  class_name: 'Airport',  foreign_key: 'from_airport_id'
   belongs_to  :to_airport,    class_name: 'Airport',  foreign_key: 'to_airport_id'
+  has_many    :bookings
+  has_many    :passengers,    through: :bookings
 
   def self.search(search)
     if search

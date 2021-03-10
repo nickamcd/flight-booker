ad0917c44cb003086fb93799cc3cfb24112e6c1e
Nick McDaniel <nickamcd@gmail.com>
2021-03-10 13:09:17 -0800

    Generated Booking model and initial migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_232320) do
+ActiveRecord::Schema.define(version: 2021_03_10_205036) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

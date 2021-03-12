f4a666cdd92a1394adc0d56829f75ae4463a1cf5
Nick McDaniel <nickamcd@gmail.com>
2021-03-12 12:34:15 -0800

    Booking tickets now links to booking show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,4 +7,28 @@ class BookingsController < ApplicationController
     @num_passengers.times { @booking.passengers.build }
   end
 
+  def create
+    puts "here"
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      puts "saves"
+      flash[:notice] = 'Booking created successfully!'
+      redirect_to booking_path(@booking)
+    else
+      puts "fails"
+      flash[:alert] = 'Oh no! Something went wrong...'
+      render "new"
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :first_name, :last_name, :email])
+  end
+
 end

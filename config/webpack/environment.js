318d94d4987cd1e2b1e75e459a88aacdc86e59a1
Nick McDaniel <nickamcd@gmail.com>
2021-03-17 10:19:12 -0700

    Added bootstrap for easier styling

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+import { ProvidePlugin } from 'webpack'
+environment.plugins.append('Provide',
+  new ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
 module.exports = environment

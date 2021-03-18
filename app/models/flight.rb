3cc94f99e59ef285dd3413450df96aeaa6d04ab1
Nick McDaniel <nickamcd@gmail.com>
2021-03-18 11:14:32 -0700

    Updated active record relations to allow destroying of records

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,8 +1,8 @@
 class Flight < ApplicationRecord
-  belongs_to  :from_airport,  class_name: 'Airport',  foreign_key: 'from_airport_id'
-  belongs_to  :to_airport,    class_name: 'Airport',  foreign_key: 'to_airport_id'
-  has_many    :bookings
-  has_many    :passengers,    through: :bookings
+  belongs_to  :from_airport, class_name: 'Airport',  foreign_key: 'from_airport_id'
+  belongs_to  :to_airport, class_name: 'Airport',  foreign_key: 'to_airport_id'
+  has_many    :bookings, dependent: :destroy
+  has_many    :passengers, through: :bookings, dependent: :destroy 
 
   def self.search(search)
     if search
